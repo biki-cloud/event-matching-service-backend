4ddc08690b5c2fe9a700ced00a2a14883f8c7e5d
hibiki <hibiki123123@gmail.com>
2024-05-28 20:29:25 +0900

    change wsgi.py

--- config/settings.py
+++ config/settings.py
@@ -177,11 +177,3 @@ SPECTACULAR_SETTINGS = {
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-import os
-
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
-
-application = get_wsgi_application()
-app = application
